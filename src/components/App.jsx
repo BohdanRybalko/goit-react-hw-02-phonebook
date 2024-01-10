d95b7d2b93f04903dfcafd4edfc4ffc3c4fd2a2e
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-10 10:28:13 +0200

    save app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import React, { useState } from 'react';
+
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const deleteContact = (id) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} addContact={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={setFilter} />
+      <ContactList contacts={contacts} filter={filter} onDeleteContact={deleteContact} />
     </div>
   );
 };
+
+export default App;
